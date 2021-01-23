6db7be8a3ebd7aea35783c0ae7ef1020b241d98b
Ethan Eshed <eitan220503@gmail.com>
2021-01-23 23:51:53 +0200

    Who's gotta be kidding? Nigga

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input;
 using System.Threading;
 using System.Collections.Generic;
 using System;
+using Newtonsoft.Json.Linq;
 
 namespace Game1
 {
@@ -11,11 +12,11 @@ namespace Game1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        public enum champions {Feng, Njal };
+        public enum champions {Feng, Ronin };
 
         private Sprite background;
 
-        public static Dictionary<champions,Dictionary<string, Animation>> animations;
+        public static Dictionary<champions, Dictionary<string, Animation>> animations;
 
         public static List<Sprite> sprites; 
         public Game1()
@@ -38,18 +39,18 @@ namespace Game1
             {
                 { champions.Feng, new Dictionary<string, Animation>()
 
-                {{"RunLeft", new Animation(Content.Load<Texture2D>("ronin/ronin_walk"), 20) },
-                 { "RunRight", new Animation(Content.Load<Texture2D>("ronin/ronin_walk"), 20) },
-                 { "JumpRight", new Animation(Content.Load<Texture2D>("test/Jump_Right"), 2) },
-                 { "JumpLeft", new Animation(Content.Load<Texture2D>("test/Jump_Left"), 2) },
-                 { "FallRight", new Animation(Content.Load<Texture2D>("test/Fall_Right"), 2) },
-                 { "FallLeft", new Animation(Content.Load<Texture2D>("test/Fall_Left"), 2) },
-                 { "IdleRight", new Animation(Content.Load<Texture2D>("test/Idle_Right"), 8) },
-                 { "IdleLeft", new Animation(Content.Load<Texture2D>("test/Idle_Left"), 8) },
-                 { "Attack1Right", new Animation(Content.Load<Texture2D>("test/Attack1_Right"), 6) },
-                 { "AttackRight", new Animation(Content.Load<Texture2D>("test/Attack_Right"), 12) },
-                 { "Attack1Left", new Animation(Content.Load<Texture2D>("test/Attack1_Left"), 6) },
-                 { "AttackLeft", new Animation(Content.Load<Texture2D>("test/Attack_Left"), 12) }} }
+                {{"RunLeft", new Animation(Content.Load<Texture2D>("Feng/Run_Left"), 8) },
+                 { "RunRight", new Animation(Content.Load<Texture2D>("Feng/Run_Right"), 8) },
+                 { "JumpRight", new Animation(Content.Load<Texture2D>("Feng/Jump_Right"), 2) },
+                 { "JumpLeft", new Animation(Content.Load<Texture2D>("Feng/Jump_Left"), 2) },
+                 { "FallRight", new Animation(Content.Load<Texture2D>("Feng/Fall_Right"), 2) },
+                 { "FallLeft", new Animation(Content.Load<Texture2D>("Feng/Fall_Left"), 2) },
+                 { "IdleRight", new Animation(Content.Load<Texture2D>("Feng/Idle_Right"), 8) },
+                 { "IdleLeft", new Animation(Content.Load<Texture2D>("Feng/Idle_Left"), 8) },
+                 { "Attack1Right", new Animation(Content.Load<Texture2D>("Feng/Attack1_Right"), 6) },
+                 { "AttackRight", new Animation(Content.Load<Texture2D>("Feng/Attack_Right"), 12) },
+                 { "Attack1Left", new Animation(Content.Load<Texture2D>("Feng/Attack1_Left"), 6) },
+                 { "AttackLeft", new Animation(Content.Load<Texture2D>("Feng/Attack_Left"), 12) }} }
             };
 
             //animations[champions.Feng]["IdleRight"].Hitboxes;
--- Game1/Managers/HitBoxFileManager.cs
+++ Game1/Managers/HitBoxFileManager.cs
@@ -3,52 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json.Linq;
 
 namespace Game1
 {
     class HitBoxFileManager
     {
-        private string filePath = "C:\\Users\\משתמש\\Desktop\\Game1\\Game1\\hitboxes.txt";
-
-        private string data;
+        private JObject jsonData;
+        private Dictionary<Game1.champions, string> types;
         public HitBoxFileManager()
         {
-            data = System.IO.File.ReadAllText(filePath);
+            jsonData = JObject.Parse(File.ReadAllText(@"..\..\..\Hitboxes.json"));
+            types = new Dictionary<Game1.champions, string>() { { Game1.champions.Feng, "Feng" }, { Game1.champions.Ronin, "Ronin" } };
         }
 
-        public List<List<Hitbox>> AquireSingleData(GraphicsDevice dev, int frameWidth, int frameHeight) // for each animation
+        public void AquireData(GraphicsDevice dev, int frameWidth, int frameHeight) // for each animation
         {
-            List<List<Hitbox>> res = new List<List<Hitbox>>();
-
-            string green = data.Substring(0, data.IndexOf("#RED#"));
-            string red = data.Substring(data.IndexOf("#RED#"));
-
-            string[] greenHitBoxes = green.Split("FRAME");
-            string[] redHitBoxes = red.Split("FRAME");
-
-            string[] tmpRed;
-            string[] tmpGreen;
-            List<Hitbox> tmpList;
-
-            for (int i = 0;i<greenHitBoxes.Length;i++)
+            dynamic tmpAnimation;
+            dynamic tmpHitbox;
+            foreach (var animationData in Game1.animations)
             {
-                tmpGreen = greenHitBoxes[i].Split('\n');
-                tmpRed = redHitBoxes[i].Split('\n');
-                tmpList = new List<Hitbox>();
-
-                for (int j = 2; j < tmpGreen.Length - 1; j++) 
+                tmpAnimation = jsonData[types[animationData.Key]];
+                foreach (var frameData in tmpAnimation)
                 {
-                    tmpList.Add(new Hitbox(frameWidth, frameHeight, tmpGreen[j], dev));
+                    
                 }
-                for (int k = 2; k < tmpRed.Length - 1; k++)
-                {
-                    tmpList.Add(new Hitbox(frameWidth, frameHeight, tmpRed[k], dev));
-                }
-
-                res.Add(tmpList);
             }
-
-            return res;
+            
         }
 
     }
