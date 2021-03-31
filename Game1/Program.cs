524a32b0bccb6c25b15fcbd316cbe90ae3809af0
Ethan Eshed <eitan220503@gmail.com>
2021-03-31 19:31:57 +0300

    another small touch

--- Game1/Program.cs
+++ Game1/Program.cs
@@ -17,6 +17,8 @@ namespace Game1
             using (var startMenu = new StartMenu())
                 startMenu.Run();
 
+            if(StartMenu.champion == "none") System.Environment.Exit(0);
+
             using (var game = new Game1())
                 game.Run();
         }
